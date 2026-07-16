fb409e4b26845392866a26691d1a1b2ad87f1ffb
geraldof2026 <sunopereira38@gmail.com>
2026-07-16 01:36:54 +0000

    29

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' // <-- ESTA É A LINHA MÁGICA QUE PUXA AS CORES
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext'; // Importe o seu Provider
+import './index.css'; // O seu CSS do Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider> {/* <-- O AuthProvider tem que abraçar o App */}
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
